635a3170ed073ad6643a4d862e512bd5a53384b0
Tai Lee <real.human@mrmachine.net>
2016-07-28 10:09:13 +1000

    Fix typo.

--- transmission/bin/entrypoint.sh
+++ transmission/bin/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 cd /opt/transmission/var
 
 # Default config.
-[[ ! -f config.yml ]] && dockerize -template ../config.tmlp.yml:config.yml
+[[ ! -f config.yml ]] && dockerize -template ../config.tmpl.yml:config.yml
 [[ ! -f settings.json ]] && cp ../settings.json .
 
 exec "${@:-sh}"
