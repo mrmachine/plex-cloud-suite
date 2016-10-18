af6b1cca303ea967b3513e218d05de79a3534595
Tai Lee <real.human@mrmachine.net>
2016-10-19 09:17:25 +1100

    Remove broken certificate symlinks before generating a self-signed certificate.

--- bin/nginx.sh
+++ bin/nginx.sh
@@ -7,7 +7,8 @@ cd /opt/var/ssl
 
 # Create a self signed default certificate, so nginx can start before we have
 # a real certificate.
-if [[ ! -f ca.pem || ! -f key.pem ]]; then
+if [[ ! -s ca.pem || ! -s key.pem ]]; then
+	rm -f ca.pem key.pem
 	openssl req -x509 -newkey rsa:2048 -keyout key.pem -out ca.pem -days 90 -nodes -subj "/CN=*/O=Plex Cloud EncFS/C=US"
 fi
 
