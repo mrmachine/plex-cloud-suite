93cdbdc20f073ea1a0e08149fb9cea7567485322
Tai Lee <real.human@mrmachine.net>
2016-10-30 21:02:06 +1100

    Revert "Remove UnionFS that combined local and ACD storage. Just use `local-storage` and `acd-storage` explicitly, and copy processed media to `acd-storage` immediately."
    678bfd0f21574534878ca28027fd00b1739cfb72

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -19,6 +19,12 @@ mkdir -p "$ACD_CLI_CACHE_PATH"
 mkdir -p /mnt/acd
 mkdir -p /mnt/acd-storage
 mkdir -p /mnt/local-storage
+mkdir -p /mnt/local-storage/'Home Videos'
+mkdir -p /mnt/local-storage/Movies
+mkdir -p /mnt/local-storage/Music
+mkdir -p /mnt/local-storage/Photos
+mkdir -p /mnt/local-storage/'TV Shows'
+mkdir -p /mnt/storage
 mkdir -p /opt/var/couchpotatoserver
 mkdir -p /opt/var/sickrage
 
@@ -43,12 +49,10 @@ elif [[ -z "$(mount | grep /mnt/acd-storage)" ]]; then
 	encfs --extpass=extpass.sh "/mnt/acd/$ACD_STORAGE_DIR" /mnt/acd-storage
 fi
 
-# Create required storage directories.
-mkdir -p /mnt/acd-storage/'Home Videos'
-mkdir -p /mnt/acd-storage/Movies
-mkdir -p /mnt/acd-storage/Music
-mkdir -p /mnt/acd-storage/Photos
-mkdir -p /mnt/acd-storage/'TV Shows'
+# Mount UnionFS filesystem.
+if [[ -z "$(mount | grep /mnt/storage)" ]]; then
+	unionfs-fuse -o cow /mnt/local-storage=RW:/mnt/acd-storage=RW /mnt/storage
+fi
 
 # Generate CouchPotatoServer API key.
 if [[ ! -f /opt/var/couchpotatoserver/api_key.txt ]]; then
