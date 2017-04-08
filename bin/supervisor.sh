106c62cab7ba9ddf79e6a642ea92ea274738d478
Tai Lee <real.human@mrmachine.net>
2017-04-08 22:27:28 +1000

    Log to supervisord directory and create directory on startup.

--- bin/supervisor.sh
+++ bin/supervisor.sh
@@ -3,6 +3,7 @@
 set -e
 
 if [[ -z "$@" ]]; then
+	mkdir -p /opt/var/logs/supervisord
 	exec supervisord --configuration /opt/etc/supervisor/supervisord.conf
 else
 	exec supervisorctl --configuration /opt/etc/supervisor/supervisord.conf "$@"
