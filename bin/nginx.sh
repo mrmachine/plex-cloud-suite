57b7279492a1cfdf705990388cd3da42238c465d
Tai Lee <real.human@mrmachine.net>
2017-04-09 00:55:46 +1000

    Change EncFS references to Suite.

--- bin/nginx.sh
+++ bin/nginx.sh
@@ -9,7 +9,7 @@ cd /opt/var/ssl
 # a real certificate.
 if [[ ! -s ca.pem || ! -s key.pem ]]; then
 	rm -f ca.pem key.pem
-	openssl req -x509 -newkey rsa:2048 -keyout key.pem -out ca.pem -days 90 -nodes -subj "/CN=*/O=Plex Cloud EncFS/C=US"
+	openssl req -x509 -newkey rsa:2048 -keyout key.pem -out ca.pem -days 90 -nodes -subj "/CN=*/O=Plex Cloud Suite/C=US"
 fi
 
 exec nginx -c "/opt/etc/nginx.conf" -g "worker_processes auto;" "$@"
