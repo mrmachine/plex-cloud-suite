ea66b9011bbd6d60f003079e335d0345949948cc
Tai Lee <real.human@mrmachine.net>
2017-04-11 00:49:51 +1000

    Unmount remote storage on exit. Do nothing if already mounted.

--- bin/rclone.sh
+++ bin/rclone.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-# Get config from environment.
-mkdir -p /root/.config/rclone
-echo "$RCLONE_CONF" > /root/.config/rclone/rclone.conf
+if ! (mount | grep -q fuse.rclone); then
+	# Get config from environment.
+	mkdir -p /root/.config/rclone
+	echo "$RCLONE_CONF" > /root/.config/rclone/rclone.conf
 
-# Create mount point.
-mkdir -p /mnt/storage
+	# Create mount point.
+	mkdir -p /mnt/storage
 
-# Unmount before re-mounting.
-if mount | grep -q fuse.rclone; then
-	fusermount -uz /mnt/storage
+	# Unmount on exit.
+	trap 'fusermount -quz /mnt/storage' INT TERM EXIT
+
+	# Mount.
+	rclone mount \
+		--stats 1s \
+		-v \
+		remote: /mnt/storage
 fi
-
-# Mount.
-rclone mount \
-	--stats 1s \
-	-v \
-	remote: /mnt/storage
